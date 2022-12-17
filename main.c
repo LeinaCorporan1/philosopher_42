6a4eaee472be40b7757b31fcdcb18569c246f84c
corporan <leina.corporan123@gmail.com>
2022-12-17 06:27:42 +0100

    ft_usleep

--- main.c
+++ main.c
@@ -33,6 +33,8 @@ int	ft_atoi(const char *str)
 	return (res * sign);
 }
 
+
+
 int	init_fork(t_stat *data)
 {
 	int	i;
@@ -44,6 +46,8 @@ int	init_fork(t_stat *data)
 			return (0);
 		i++;
 	}
+	if (pthread_mutex_init(&data->print, NULL))
+		return (0);
 	return (1);
 }
 
@@ -58,6 +62,8 @@ int	init_philo(t_stat *data)
 	while(i < data->nb_philo)
 	{
 		data->philo[i].id = i + 1;
+		data->philo[i].is_dead = 0;
+		// data->philo[i].is_dead = 0;
 		data->philo[i].l_fork = i;
 		if (i != 0)
 			data->philo[i].r_fork = (i + 1) % data->nb_philo;
@@ -68,6 +74,35 @@ int	init_philo(t_stat *data)
 		i++;
 	}
 	return (1);
+}
+
+long long	time_phi(void)
+{
+	struct timeval time;
+	long long i;
+
+	if (gettimeofday(&time, NULL) == -1)
+		return (-1);
+	 return((time.tv_sec * 100) + (time.tv_usec / 1000));
+	 printf ("i  = %lli tv_sec = %li tv_unsec = %li \n", i , (time.tv_sec * 100), (time.tv_usec / 1000));
+}
+
+void	ft_usleep(long long time_info)
+{
+	long long	start;
+
+	start = time_phi();
+	while (time_phi() - start <= time_info)
+		usleep(time_info / 10);
+}
+
+
+
+void	print(long long time, t_philo *philo, char *str)
+{
+	pthread_mutex_lock(&philo->data->print);
+	printf("%lli %d %s\n", time - philo->data->act_time, philo->id, str);
+	pthread_mutex_unlock(&philo->data->print);
 }
 
 int	init_stat(char **av, t_stat *data)
@@ -90,23 +125,59 @@ int	init_stat(char **av, t_stat *data)
 		return (0);
 }
 
-void	journey
+int	as_to_stop(t_philo *philosophers)
+{
+		int	i;
+	t_philo *philo;
 
-void	*test(void *philosopher)
+	i = 0;
+	philo = (t_philo *)philosophers;
+	if (philo->is_dead)
+		return (0);
+}
+
+
+
+void	test(void *philosophers)
 {
 	t_philo *philo;
 
-	philo = (t_philo *)philosopher;
+	philo = (t_philo *)philosophers;
 	if (philo->id % 2 == 0)
 		usleep(15000);
 	pthread_mutex_lock(&philo->data->fork[philo->l_fork]);
-	printf("philo {%d} is taken left fork [%d]\n",philo-> id, philo->l_fork);
+	// printf("philo {%d} is taken left fork [%d]\n",philo-> id, philo->l_fork);
+	print(time_phi(), philo, "has taken a fork");
 	pthread_mutex_lock(&philo->data->fork[philo->r_fork]);
-	printf("philo {%d} is taken right fork [%d]\n",philo-> id, philo->r_fork);
-	
+	print(time_phi(), philo, "has taken a fork");
+
+	// printf("philo {%d} is taken right fork [%d]\n",philo-> id, philo->r_fork);
+	print(time_phi(), philo, "is eating");
+	ft_usleep(philo->data->eat);
+	philo->as_eaten++;
 	pthread_mutex_unlock(&philo->data->fork[philo->l_fork]);
 	pthread_mutex_unlock(&philo->data->fork[philo->r_fork]);
 
+}
+
+int	found_dead (t_philo *philosopher)
+{
+	if ()
+}
+
+void	*journey(void *philosophers)
+{
+	int	i;
+	t_philo *philo;
+
+	i = 0;
+	philo = (t_philo *)philosophers;
+	while (philo->as_eaten != philo->data->m_eat)
+	{
+		test(philo);
+		//printf("philo[%i] as eaten %d\n",philo->id, philo->as_eaten);
+
+	}
 }
 
 int main(int ac, char **av)
@@ -128,14 +199,16 @@ int main(int ac, char **av)
 		i++;
 	}
 	 i  = 0;
+	 data.act_time = time_phi();
 	while(i < data.nb_philo)
 	{
 		pthread_mutex_init(&data.fork[i], NULL);
-		if (pthread_create(&data.philo[i].phi,NULL, &test, &data.philo[i]) != 0)
+		if (pthread_create(&data.philo[i].phi,NULL, &journey, &data.philo[i]) != 0)
 			return (0);
 		printf ("created at %d\n",i);
 		i ++;
 	}
+	// printf("OUTSIIIDEE\n");
 	i = 0;
 
 	while(i < data.nb_philo)
--- philo.h
+++ philo.h
@@ -5,26 +5,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/time.h>
 
 typedef	struct s_philo
 {
-	int r_fork;
-	int l_fork;
+	int	r_fork;
+	int	l_fork;
 	int	id;
+	int	is_dead;
+	int	as_eaten;
 	struct s_stat	*data;
 	pthread_t phi;
 } t_philo;
 
 typedef struct s_stat
 {
-	int	eat;
-	int	sleep;
-	int	think;
-	int	nb_philo;
-	int	m_eat;
-	int	die;
+	long long	eat;
+	long long	sleep;
+	long long	think;
+	long long	nb_philo;
+	long long	m_eat;
+	long long	die;
+	long long	philo_died;
+	long long	act_time;
 	t_philo	*philo;
-	pthread_mutex_t *fork;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	print;
 }	t_stat;
 
 #endif
